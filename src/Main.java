5d68f3bf28444f8eec6aa5b4ba2f8236619bf3ff
Pranav Lejith <pranavlejith@gmail.com>
2024-10-07 09:03:58 +0530

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ import java.util.Scanner;  // Import the Scanner class
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Created by Amphibiar(Pranav Lejith)");
+        System.out.println("Created by Amphibiar(Pranav Lejith)(7th October 2024)");
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter username");
 
