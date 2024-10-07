d1a34a3d5c8e69c66da8ac1c58c8de8cd60623ca
Pranav Lejith <pranavlejith@gmail.com>
2024-10-07 09:03:27 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        System.out.println("Hello world!");
+//        int x;
+//        x=15;
+//        for(int i=1; i<=x; i++)
+//        {
+//            System.out.println(" i= "  + i);
+//        }
+//        String greeting = "Hello";
+//        int greetLength = greeting.length();
+//        System.out.println(greetLength);
+//
+//    }
+//}
+
+import java.util.Scanner;  // Import the Scanner class
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Created by Amphibiar(Pranav Lejith)");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);
+        System.out.println("Length of username is "+ userName.length()); // Output user input
+    }
+}
